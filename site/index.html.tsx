56fb31fe3f6ec0422008aa8b3032f601d74e6ce6
sdegutis <sbdegutis@gmail.com>
2024-08-15 17:26:52 -0500

    Simple example.

--- site/index.html.tsx
+++ site/index.html.tsx
@@ -1,6 +1,20 @@
-export default <>
-  {`<!doctype html>`}
-  <html style='background:#222;color:#ddd'>
-    <b>Hello <i>world!</i></b>
-  </html>
-</>;
+import { Em } from "./em.js";
+
+function Html(attrs: any, children: any) {
+  return <>
+    {`<!doctype html>`}
+    <html>
+      <head />
+      <body>
+        {children}
+      </body>
+    </html>
+  </>;
+}
+
+export default <Html>
+  <link rel='stylesheet' href='style.css' />
+  <script type='module' src='client.js' />
+  <p>Hello from the <Em>SSG</Em>.</p>
+  <div id='root' />
+</Html>;
